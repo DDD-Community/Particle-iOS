c12d3a4ba84ddc4ea76c30cdbc6dbcfd06329d87
Peter <ghdtjrgus333@naver.com>
2023-07-12 13:32:02 +0900

    feat. PaddingLabel 추가

--- Particle/Particle/Main/AddArticle/OrganizingSenctence/View/PaddingLabel.swift
+++ Particle/Particle/Main/AddArticle/OrganizingSenctence/View/PaddingLabel.swift
@@ -5,4 +5,25 @@
 //  Created by Sh Hong on 2023/07/12.
 //
 
-import Foundation
+import UIKit
+
+class PaddingLabel: UILabel {
+    
+    var topInset: CGFloat = 0
+    var bottomInset: CGFloat = 0
+    var leftInset: CGFloat = 0
+    var rightInset: CGFloat = 0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.height += topInset + bottomInset
+        contentSize.width += leftInset + rightInset
+        
+        return contentSize
+    }
+}
