1df1f213f619a5a2a081b177a9819e220ba559dc
Peter <ghdtjrgus333@naver.com>
2023-07-12 13:32:25 +0900

    feat. 문장 선택 및 구성 ui

--- Particle/Particle/Main/AddArticle/OrganizingSenctence/OrganizingSentenceViewController.swift
+++ Particle/Particle/Main/AddArticle/OrganizingSenctence/OrganizingSentenceViewController.swift
@@ -8,6 +8,8 @@
 import RIBs
 import RxSwift
 import UIKit
+import SnapKit
+import RxCocoa
 
 protocol OrganizingSentencePresentableListener: AnyObject {
     // TODO: Declare properties and methods that the view controller can invoke to perform
@@ -18,4 +20,87 @@ protocol OrganizingSentencePresentableListener: AnyObject {
 final class OrganizingSentenceViewController: UIViewController, OrganizingSentencePresentable, OrganizingSentenceViewControllable {
 
     weak var listener: OrganizingSentencePresentableListener?
+    private var disposeBag: DisposeBag = .init()
+    
+    enum Metric {
+        enum Title {
+            static let topMargin = 12
+            static let leftMargin = 20
+        }
+        
+        enum TableView {
+            static let topMargin = 23
+            static let horizontalMargin = 20
+        }
+    }
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 19, weight: .semibold)
+        label.text = "문장 순서와 대표 문장을 설정하세요"
+        label.textColor = .white
+        return label
+    }()
+    
+    private let sentenceTableView: UITableView = {
+        let table = UITableView()
+        table.register(SentenceTableViewCell.self)
+        table.backgroundColor = .clear
+        table.alwaysBounceVertical = false
+        table.rowHeight = UITableView.automaticDimension
+        table.estimatedRowHeight = 50
+        return table
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.modalPresentationStyle = .fullScreen
+        self.view.backgroundColor = .black
+        addSubviews()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Observable.of([
+            "그렇게 쓴 글은 매일 사회관계망서비스(SNS)에 남기기도 하고, 모아서 책으로 내기도 한다.",
+            "개인적으로 나는 매일 글쓰는 일을 습관처럼 하고 있다. ",
+            "그러나 처음부터 글을 습관처럼 매일 쓸 수 있었던 건 아니다.",
+            "하지만 당시에는 작가가 되고 싶다는 열망 하나를 갖고 있었고, 그 젊은 열망에 따라 어떻게든 무엇이든 글을 쓰고자 했다. ",
+            "많은 사람들이 내게 그렇게 매일 글을 쓸 수 있는 비결을 물어보곤 하는데, 나는 그저 습관이 되어버렸다고 말할 수밖에 없다."
+                      ])
+        .bind(to: sentenceTableView.rx.items(cellIdentifier: SentenceTableViewCell.defaultReuseIdentifier, cellType: SentenceTableViewCell.self)) { index, item, cell in
+            cell.setCellData(item)
+        }
+        .disposed(by: disposeBag)
+    }
+    
+    // MARK: - Add Subviews
+    private func addSubviews() {
+        [titleLabel,
+        sentenceTableView]
+            .forEach {
+                self.view.addSubview($0)
+            }
+    }
+    
+    // MARK: - Layout
+    private func layout() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).inset(Metric.Title.topMargin)
+            make.left.equalTo(self.view.safeAreaLayoutGuide).inset(Metric.Title.leftMargin)
+        }
+        
+        sentenceTableView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(Metric.TableView.topMargin)
+            make.left.right.equalToSuperview().inset(Metric.TableView.horizontalMargin)
+            make.bottom.equalToSuperview()
+        }
+    }
 }
--- Particle/Particle/Main/AddArticle/OrganizingSenctence/View/SentenceTableViewCell.swift
+++ Particle/Particle/Main/AddArticle/OrganizingSenctence/View/SentenceTableViewCell.swift
@@ -5,4 +5,58 @@
 //  Created by Sh Hong on 2023/07/11.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class SentenceTableViewCell: UITableViewCell {
+    
+    private enum Metric {
+        static let bottomMargin: CGFloat = 10
+        static let labelVerticalInset: CGFloat = 16
+        static let labelHorizontalInset: CGFloat = 14
+        static let labelCornerRadius: CGFloat = 12
+    }
+    
+    private let sentenceLabel: PaddingLabel = {
+        let label = PaddingLabel()
+        label.topInset = Metric.labelVerticalInset
+        label.bottomInset = Metric.labelVerticalInset
+        label.leftInset = Metric.labelHorizontalInset
+        label.rightInset = Metric.labelHorizontalInset
+        label.font = .systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.textColor = .white
+        label.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.1)
+        label.layer.cornerRadius = Metric.labelCornerRadius
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = .clear
+        self.selectionStyle = .none
+        
+        self.addSubview(sentenceLabel)
+        sentenceLabel.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(Metric.bottomMargin)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        sentenceLabel.preferredMaxLayoutWidth = sentenceLabel.frame.width
+    }
+    
+    func setCellData(_ data: String) {
+        sentenceLabel.text = data
+        layoutSubviews()
+    }
+}
