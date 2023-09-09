d4409f17698bf43396a32ab2430d891dbd1cbb29
wongbingg <wb277@naver.com>
2023-09-09 10:06:02 +0900

    feat : DateManager 추가

---
+++ Particle/Particle/Utils/DateManager.swift
@@ -0,0 +1,26 @@
+//
+//  DateManager.swift
+//  Particle
+//
+//  Created by 이원빈 on 2023/09/03.
+//
+
+import Foundation
+
+final class DateManager {
+    static let shared = DateManager()
+    private let dateFormatter: DateFormatter
+    
+    private init() {
+        self.dateFormatter = DateFormatter()
+        self.dateFormatter.locale = Locale(identifier: "ko")
+    }
+    
+    func convert(previousDate: String, to format: String) -> String {
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        guard let date = dateFormatter.date(from: previousDate) else { return "" }
+        dateFormatter.dateFormat = format
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
+}
